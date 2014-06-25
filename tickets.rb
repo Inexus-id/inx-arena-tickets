ee96651621729c547bc5dc3c185f206aa70e09fc
Juan Gabriel Fraire López <groteck@gmail.com>
2014-06-25 09:52:37 +0100

    change tickets dimensions

--- tickets.rb
+++ tickets.rb
@@ -1,14 +1,10 @@
 #!/usr/bin/env ruby 
 require 'nokogiri'
 require 'csv'
-# Create tickets and pdf files directory
-`mkdir tickets pdf`
-# Delete previous pdf files
-`rm pdf/*`
-
 class Ticket
-  def initialize(name)
+  def initialize(name, number)
     @name = name
+    @number = number
   end
 
   def build
@@ -18,34 +14,38 @@ class Ticket
     doc = Nokogiri::XML(f)
     f.close
     doc.at_css("#tspan6398").content = @name
-    File.open("entradas/#{@name}.svg", 'a') { |f| f.write(doc.to_xml) }
-    "tickets/#{@name}.svg"
+    File.open("tickets/#{@number}.svg", 'a') { |f| f.write(doc.to_xml) }
+    "tickets/#{@number}.svg"
   end
 end
 
 # CSV data should has name data.csv
 # Use the first columns for add to the svg file 
 # and save with column_content.svg into tickets directory  
-svg_files = []
+uniq_names = []
 CSV.foreach("data.csv") do |row|
-  t = Ticket.new(row[0])
+  uniq_names << row[0]
+end
+
+svg_files = []
+index = 0
+uniq_names.uniq.map{|n| (n.length > 20 ? n[/(.*)\s/,1] : n)}.each do |row|
+  t = Ticket.new(row, index)
   svg_files << t.build
+  index += 1
 end
 
 # Create with imagemagick the image matrix
 
 # Set border for images
-border = " -bordercolor '#fff' -border 3x3 "
+border = " -bordercolor '#ffd374' -border 2x2 "
 
 # Create pdf files with 42 tickets per page
-svg_files.each_slice(42).with_index do | images, page |
+svg_files.each_slice(30).with_index do | images, page |
   lines = []
-  images.each_slice(7) do | row |
+  images.each_slice(6) do | row |
     lines << row.join(' ') + border
   end
   image_matrix = lines.map {| line | "'(' #{line} +append ')'"}.join(' ')
   `convert #{image_matrix} -append pdf/page-#{page + 1}.pdf`
 end
-
-# Remove tickets files
-`rm tickets/*`
